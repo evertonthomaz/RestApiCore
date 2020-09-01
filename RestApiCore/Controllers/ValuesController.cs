c26720de283b61a4d27cf7b1bab13865500e5fe5
Everton Thomáz <everton.thomaz@outlook.com>
2020-09-01 15:17:15 -0300

    teste git

--- RestApiCore/Controllers/ValuesController.cs
+++ RestApiCore/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace RestApiCore.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value12", "value2" };
         }
 
         // GET api/values/5
